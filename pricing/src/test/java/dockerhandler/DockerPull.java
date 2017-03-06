fe79bf2549cc076a28c02cf56a87e5dcc0f60ec8
Rajasekhar Asapu <RAsapu@wfscorp.com>
2017-03-06 14:06:58 -0500

    Converted execute method to static

--- pricing/src/test/java/dockerhandler/DockerPull.java
+++ pricing/src/test/java/dockerhandler/DockerPull.java
@@ -1,18 +1,12 @@
 package dockerhandler;
 
-import org.junit.Test;
-
 import java.io.File;
-
-import static org.bouncycastle.asn1.x500.style.RFC4519Style.c;
 
 public class DockerPull extends DockerConstants{
 
     String[] gitrepos={priceUi_gitRepo,priceEngine_gitRepo,priceDataMock_gitRepo,priceConfigServices_gitRepo};
 
     String rootRepoDir="./Repos/";
-
-    ExecuteCommands ec=new ExecuteCommands();
 
     /*
       To do : Handle exceptions
@@ -23,51 +17,39 @@ public class DockerPull extends DockerConstants{
 
             if(rootDir.exists())
             {
-              ec.exec("rm -r " + rootRepoDir);
+              ExecuteCommands.exec("rm -r " + rootRepoDir);
             }
 
             rootDir.mkdir();
-
             /*
-              pull repositories
-            */
+             *  pull repositories
+             */
             for(String tempUrl:gitrepos)
             {
                 String[] folder= tempUrl.split("/");
-
                 String path=rootRepoDir+folder[folder.length-1];
 
-                ec.exec("mkdir "+path);
-                ec.exec("git clone "+tempUrl+" "+path);
+                ExecuteCommands.exec("mkdir "+path);
+                ExecuteCommands.exec("git clone "+tempUrl+" "+path);
             }
 
     }
 
-    public void buildPriceUi()throws Exception
-    {
-        /*
-         To do : Build the price config ui
-         */
-//        String[] folder= priceUi_gitRepo.split("/");
-//        String path=rootRepoDir+folder[folder.length-1];
-//
-//        Runtime.getRuntime().exec("sh -c \"cd ./Repos/price-configuration-ui.git\"");
-//        ec.exec("ls -l");
-////        ec.exec("npm install -g angular-cli");
-////        ec.exec("ng build "+path);
-    }
+    /*
+     * Method to copy index lookup table from dev to the price db container
+     */
 
     public void loadIndexLookupTables()throws Exception
     {
-        ec.exec("docker exec -it runPriceDB4.0 bash");
-        ec.exec("cqlsh 10.172.5.62;");
-        ec.exec("COPY wfs_pricing.index_lookup TO './indexLookUp.csv';");
+        ExecuteCommands.exec("docker exec -it runPriceDB4.0 bash");
+        ExecuteCommands.exec("cqlsh 10.172.5.62;");
+        ExecuteCommands.exec("COPY wfs_pricing.index_lookup TO './indexLookUp.csv';");
         Thread.sleep(4000);
-        ec.exec("exit");
-        ec.exec("cqlsh");
-        ec.exec("COPY wfs_pricing.index_lookup FROM './indexLookUp.csv';");
+        ExecuteCommands.exec("exit");
+        ExecuteCommands.exec("cqlsh");
+        ExecuteCommands.exec("COPY wfs_pricing.index_lookup FROM './indexLookUp.csv';");
         Thread.sleep(4000);
-        ec.exec("exit");
+        ExecuteCommands.exec("exit");
 
     }
 }

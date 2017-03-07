db95eda54236131ab144adee944ed3a6aa65ae52
Rajasekhar Asapu <RAsapu@wfscorp.com>
2017-03-07 10:02:29 -0500

    Moved git repo urls to properties file

--- pricing/src/test/java/dockerhandler/DockerPull.java
+++ pricing/src/test/java/dockerhandler/DockerPull.java
@@ -1,55 +1,77 @@
 package dockerhandler;
 
+import org.junit.Test;
 import java.io.File;
+import java.io.InputStream;
+import java.util.Properties;
 
-public class DockerPull extends DockerConstants{
+public class DockerPull{
 
-    String[] gitrepos={priceUi_gitRepo,priceEngine_gitRepo,priceDataMock_gitRepo,priceConfigServices_gitRepo};
+	private String[] gitrepos=new String[4] ;
+	private String rootRepoDir = "./Repos/";
+	File resourceFile;
+	String resourceFilePath;
+	InputStream in;
+	Properties props;
+	/*
+	 * To do : Handle exceptions
+	 */
+	@Test
+	public void cloneRepositories() throws Exception {
 
-    String rootRepoDir="./Repos/";
+		/*
+		 * Load properties file
+		 */
+		props=new Properties();
+		in=getClass().getClassLoader().getResourceAsStream("repos.properties");
 
-    /*
-      To do : Handle exceptions
-     */
-    public void cloneRepositories()throws Exception
-    {
-            File rootDir=new File(rootRepoDir);
+		if(in!=null)
+		{
+			props.load(in);
+		}
+		else
+		{
+			throw new Exception("Property file not found:repos.properties");
+		}
+		gitrepos[0]=props.getProperty("priceUi_gitRepo");
+		gitrepos[1]=props.getProperty("priceEngine_gitRepo");
+		gitrepos[2]=props.getProperty("priceConfigServices_gitRepo");
+		gitrepos[3]=props.getProperty("priceDataMock_gitRepo");
 
-            if(rootDir.exists())
-            {
-              ExecuteCommands.exec("rm -r " + rootRepoDir);
-            }
+		File rootDir = new File(rootRepoDir);
 
-            rootDir.mkdir();
-            /*
-             *  pull repositories
-             */
-            for(String tempUrl:gitrepos)
-            {
-                String[] folder= tempUrl.split("/");
-                String path=rootRepoDir+folder[folder.length-1];
+		if (rootDir.exists()) {
+			ExecuteCommands.exec("rm -r " + rootRepoDir);
+		}
 
-                ExecuteCommands.exec("mkdir "+path);
-                ExecuteCommands.exec("git clone "+tempUrl+" "+path);
-            }
+		rootDir.mkdir();
+		/*
+		 * pull repositories
+		 */
+		for (String tempUrl : gitrepos) {
+			String[] folder = tempUrl.split("/");
+			String path = rootRepoDir + folder[folder.length - 1];
 
-    }
+			ExecuteCommands.exec("mkdir " + path);
+			ExecuteCommands.exec("git clone " + tempUrl + " " + path);
+		}
 
-    /*
-     * Method to copy index lookup table from dev to the price db container
-     */
+	}
 
-    public void loadIndexLookupTables()throws Exception
-    {
-        ExecuteCommands.exec("docker exec -it runPriceDB4.0 bash");
-        ExecuteCommands.exec("cqlsh 10.172.5.62;");
-        ExecuteCommands.exec("COPY wfs_pricing.index_lookup TO './indexLookUp.csv';");
-        Thread.sleep(4000);
-        ExecuteCommands.exec("exit");
-        ExecuteCommands.exec("cqlsh");
-        ExecuteCommands.exec("COPY wfs_pricing.index_lookup FROM './indexLookUp.csv';");
-        Thread.sleep(4000);
-        ExecuteCommands.exec("exit");
+	/*
+	 * Method to copy index lookup table from dev to the price db container
+	 */
 
-    }
+	public void loadIndexLookupTables() throws Exception {
+		ExecuteCommands.exec("docker exec -it runPriceDB4.0 bash");
+		ExecuteCommands.exec("cqlsh 10.172.5.62;");
+		ExecuteCommands.exec("COPY wfs_pricing.index_lookup TO './indexLookUp.csv';");
+		Thread.sleep(4000);
+		ExecuteCommands.exec("exit");
+		ExecuteCommands.exec("cqlsh");
+		ExecuteCommands.exec("COPY wfs_pricing.index_lookup FROM './indexLookUp.csv';");
+		Thread.sleep(4000);
+		ExecuteCommands.exec("exit");
+
+	}
 }

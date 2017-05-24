6e328233701f1210597026ad41ad3f95ada25500
RajaSekhar A <rasapu@wfscorp.com>
2017-05-24 18:09:17 -0400

    Added arquillian phantom js

--- src/test/java/setup/OpenBrowser.java
+++ src/test/java/setup/OpenBrowser.java
@@ -1,5 +1,7 @@
 package setup;
 
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import org.jboss.arquillian.phantom.resolver.ResolvingPhantomJSDriverService;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -13,6 +15,7 @@ import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -72,17 +75,26 @@ public class OpenBrowser {
             case HtmlUnitDriver:
                 driver = new HtmlUnitDriver(true);
             case PhantomJS:
-                DesiredCapabilities capabilities = new DesiredCapabilities();
-                PhantomJSDriverService.createDefaultService();
-                capabilities.setJavascriptEnabled(true);
-                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"phantomjs");
-                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,"--webdriver=3838 --webdriver-selenium-grid-hub" +
-                        "=http ://localhost:3839");
-                capabilities.setCapability("--ignore-ssl-errors",true);
-                driver = new PhantomJSDriver(capabilities);
-                long start = System.currentTimeMillis();
-                ((JavascriptExecutor) driver).executeAsyncScript(
-                        "window.setTimeout(arguments[arguments.length - 1], 5000);");
+//                PhantomJsDriverManager.getInstance().setup();
+//                DesiredCapabilities capabilities = new DesiredCapabilities();
+//                capabilities.setJavascriptEnabled(true);
+////                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"phantomjs");
+//                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX,true);
+//                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_PATH_PROPERTY,"2.0.0");
+//                capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,"--webdriver=3838 --webdriver-selenium-grid-hub" +
+//                        "=http ://localhost:3839");
+//                capabilities.setCapability("--ignore-ssl-errors",true);
+//                driver = new PhantomJSDriver(capabilities);
+//                long start = System.currentTimeMillis();
+//                ((JavascriptExecutor) driver).executeAsyncScript(
+//                        "window.setTimeout(arguments[arguments.length - 1], 5000);");
+                try {
+                    driver = new PhantomJSDriver(
+                            ResolvingPhantomJSDriverService.createDefaultService(),
+                            DesiredCapabilities.phantomjs());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 driver.manage().window().setSize(new Dimension(1280,1024));
         }
         return driver;

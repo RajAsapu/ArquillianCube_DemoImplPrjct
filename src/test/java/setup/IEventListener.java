20aceca86eaa4e27c03f374e3a38298aafbd6eb6
RajaSekhar A <rasapu@wfscorp.com>
2017-05-24 18:43:17 -0400

    added logs

--- src/test/java/setup/IEventListener.java
+++ src/test/java/setup/IEventListener.java
@@ -1,16 +1,21 @@
 package setup;
 
+import functions.PageCommonMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IEventListener extends AbstractWebDriverEventListener {
 
     @Override
     public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-       // ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Logger log = LoggerFactory.getLogger(PageCommonMethods.class);
+        log.debug(driver.getCurrentUrl());
+        log.debug(driver.getPageSource());
     }
 
     @Override

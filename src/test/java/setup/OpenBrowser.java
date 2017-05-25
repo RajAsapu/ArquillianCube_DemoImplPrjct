a23fddf203cc53df725ea70e140aede6f9da731c
RajaSekhar A <rasapu@wfscorp.com>
2017-05-25 14:00:18 -0400

    working with standalone chrome driver

--- src/test/java/setup/OpenBrowser.java
+++ src/test/java/setup/OpenBrowser.java
@@ -1,5 +1,6 @@
 package setup;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.PhantomJsDriverManager;
 import org.jboss.arquillian.phantom.resolver.ResolvingPhantomJSDriverService;
 import org.openqa.selenium.Dimension;
@@ -13,9 +14,12 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +30,7 @@ public class OpenBrowser {
     private EventFiringWebDriver edriver;
     private IEventListener listener;
     public static enum Open {
-        CHROME, IE, FIREFOX, HtmlUnitDriver , PhantomJS ,ChromeHeadLess
+        CHROME, IE, FIREFOX, HtmlUnitDriver , PhantomJS
     }
     public enum url {
         Pricing
@@ -50,24 +54,34 @@ public class OpenBrowser {
 
     public WebDriver getDriver(Open browser) {
         switch (browser) {
-            case ChromeHeadLess:
-
             case CHROME:
-                System.setProperty("webdriver.chrome.driver",
-                        "chromedriver");
+//                ChromeDriverManager.getInstance().setup();
+//                System.setProperty("webdriver.chrome.driver",
+//                        "chromedriver");
                 ChromeOptions options = new ChromeOptions();
                 Map<String, Object> prefs = new HashMap<String, Object>();
                 prefs.put("credentials_enable_service", false);
                 prefs.put("profile.password_manager_enabled", false);
                 options.setExperimentalOption("prefs", prefs);
-                options.addArguments("--no-sandbox");
-                options.setBinary("/usr/bin/google-chrome-unstable");
-                options.addArguments("window-size=1280x1024");
-                options.addArguments("--headless");
+//                options.addArguments("--no-sandbox");
+////                options.setBinary("/usr/bin/google-chrome-stable");
+//                options.addArguments("window-size=1280x1024");
+//                options.addArguments("--headless");
                 options.addArguments("--disable-plugins");
                 options.addArguments("--start-maximized");
                 options.addArguments("--disable-extensions");
-                driver = new ChromeDriver(options);
+                DesiredCapabilities capabilities=new DesiredCapabilities();
+                capabilities.setCapability(ChromeOptions.CAPABILITY,options);
+                capabilities.setBrowserName("chrome");
+                capabilities.acceptInsecureCerts();
+                try {
+
+                    driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+                    //driver = new ChromeDriver(capabilities);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                driver.manage().window().setSize(new Dimension(1280,1024));
                 break;
             case FIREFOX:
                 System.setProperty("webdriver.gecko.driver",
@@ -96,7 +110,7 @@ public class OpenBrowser {
                 try {
                     String[] cli_args = new String[]{ "--ignore-ssl-errors=true" ,"--ssl-certificates-path=Fortinet_CA_SSLProxy.pem"};
 
-                    DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+                    capabilities = DesiredCapabilities.phantomjs();
                     capabilities.setJavascriptEnabled(true);
                     capabilities.acceptInsecureCerts();
                     capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,cli_args);

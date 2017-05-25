6d1faa13e6dabe74a0b3a8a1b49447d66a9b031b
RajaSekhar A <rasapu@wfscorp.com>
2017-05-25 16:34:11 -0400

    Code to take screen shot

--- src/test/java/setup/IEventListener.java
+++ src/test/java/setup/IEventListener.java
@@ -6,21 +6,116 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class IEventListener extends AbstractWebDriverEventListener {
+public class IEventListener implements WebDriverEventListener {
+
+
+    @Override
+    public void beforeAlertAccept(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterAlertAccept(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterAlertDismiss(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeAlertDismiss(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeNavigateTo(String url, WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterNavigateTo(String url, WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeNavigateBack(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterNavigateBack(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeNavigateForward(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterNavigateForward(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeNavigateRefresh(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterNavigateRefresh(WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
 
     @Override
     public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-        Logger log = LoggerFactory.getLogger(PageCommonMethods.class);
-        log.debug(driver.getCurrentUrl());
-        log.debug(driver.getPageSource());
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeClickOn(WebElement element, WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterClickOn(WebElement element, WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void beforeScript(String script, WebDriver driver) {
+        new DateOperations().getScreenShot();
+    }
+
+    @Override
+    public void afterScript(String script, WebDriver driver) {
+
     }
 
     @Override
     public void onException(Throwable throwable, WebDriver driver) {
-        throwable.printStackTrace();
+        new DateOperations().getScreenShot();
     }
-
 }

b091810c801ef2f3800afab7e017532cdaaf5d86
RajaSekhar A <rasapu@wfscorp.com>
2017-06-16 15:36:57 -0400

    Changed to comnpose

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -1,36 +1,11 @@
 package runner;
 
-import com.google.common.base.Verify;
 import cucumber.api.CucumberOptions;
-import cucumber.runtime.arquillian.CukeSpace;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import net.masterthought.cucumber.Reportable;
-import org.arquillian.cube.CubeController;
-import org.arquillian.cube.CubeIp;
-import org.arquillian.cube.HostPort;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import setup.ContainerConfiguration;
-import setup.OpenBrowser;
-import setup.UpdateProperties;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RunWith(Arquillian.class)
-@CucumberOptions(
-        plugin = {"progress","html:target/cucumber-html-report", "json:target/cucumber-json-report"},
-        features = {"src/test/resources/features/"},
-        glue = {"classpath:"},
-        tags = {"@AppTestData"}
-)
 public class RunTest {
     @Test
     public void getImage()

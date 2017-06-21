d64f747f2b243737a34e5dc5c19b7207843cf28f
RajaSekhar A <rasapu@wfscorp.com>
2017-06-21 16:03:34 -0400

    Added smoke test tag

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -33,7 +33,7 @@ import static java.util.Collections.singletonList;
         plugin = {"json:target/cucumber-json-report"},
         features = {"src/test/resources/features/"},
         glue = {"classpath:"},
-        tags = {"@TestCalc"}
+        tags = {"@AppTestData,@SmokeTest"}
 )
 public class RunTest {
 

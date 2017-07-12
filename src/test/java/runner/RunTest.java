c7a3c3375e0d4d12a10e2a6fda7bddce166b7c1e
RajaSekhar A <rasapu@wfscorp.com>
2017-07-12 17:34:23 -0400

    Modified tags

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -23,7 +23,7 @@ import java.util.List;
         plugin = {"json:target/cucumber-json-report"},
         features = {"src/test/resources/features/"},
         glue = {"classpath:"},
-        tags = {"@AppTestData,@SmokeTest"}
+        tags = {"@TestCalc"}
 )
 public class RunTest {
 

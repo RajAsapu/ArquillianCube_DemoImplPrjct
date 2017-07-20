ae1cedd2e32c92f89998e695f0fd3c4fb032b44b
RajaSekhar A <rasapu@wfscorp.com>
2017-07-20 15:48:00 -0400

    Arquillian demo prjct for network test

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -1,92 +1,18 @@
 package runner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.runtime.arquillian.CukeSpace;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.arquillian.cube.CubeController;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.junit.AfterClass;
+import cucumber.runtime.arquillian.ArquillianCucumber;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import setup.ConfigureProperties;
-import setup.Constants;
-import setup.OpenBrowser;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+@RunWith(ArquillianCucumber.class)
 
-@RunWith(CukeSpace.class)
-@CucumberOptions(
-        plugin = {"json:target/cucumber-json-report"},
-        features = {"src/test/resources/features/"},
-        glue = {"classpath:"},
-        tags = {"@AppTestData"}
-)
 public class RunTest {
-
-    @ArquillianResource
-    static CubeController cubeController;
-    private static Logger log = LoggerFactory.getLogger(RunTest.class);
-    private ConfigureProperties configureProperties = new ConfigureProperties();
-
-    @AfterClass
-    public static void generateReports() {
-        ConfigureProperties props = new ConfigureProperties();
-        /*
-         * Stop the service container
-         */
-        if (System.getenv(Constants.ENV_KEY).equals(Constants.DOCKER_ENV)) {
-            cubeController.stop(ConfigureProperties.getGradleProperty("serviceContainerName"));
-            cubeController.destroy(ConfigureProperties.getGradleProperty("serviceContainerName"));
-        }
-        /*
-         * Generate report
-         */
-        OpenBrowser browser = new OpenBrowser();
-        File reportOutputDirectory = new File("target");
-        List<String> jsonFiles = new ArrayList<>();
-        jsonFiles.add("target/cucumber-json-report");
-
-        String projectName = "Pricing-e2e-tests";
-        boolean parallelTesting = false;
-
-        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
-        configuration.setParallelTesting(false);
-
-        configuration.addClassifications("Environment", System.getenv(Constants.ENV_KEY));
-        configuration.addClassifications("Browser", browser.getSelectedDriver());
-        configuration.addClassifications("Build Number", System.getenv("bamboo.buildNumber"));
-
-        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
-        reportBuilder.generateReports();
-    }
-
+    private static Logger logger = LoggerFactory.getLogger(RunTest.class);
     @Test
-    public void setEnvironment() {
-        if (System.getenv(Constants.ENV_KEY).equals(Constants.DOCKER_ENV)) {
-            /*
-             * Start the service container
-             */
-            cubeController.create(ConfigureProperties.getGradleProperty("serviceContainerName"));
-            cubeController.start(ConfigureProperties.getGradleProperty("serviceContainerName"));
-            log.info("Service Container has started");
-            /*
-             * Update pricing properties
-             */
-            configureProperties.updatePricingProperties();
-            /*
-             * Update hostConfig file in the ui container
-             */
-            configureProperties.updateHostConfig();
-            /*
-             * Display running containers
-             */
-            configureProperties.checkIfContainersAreRunning();
-            log.info("Running tests");
-        }
+    public void verifyIfJenkinsRuns()
+    {
+        logger.info("Jenkins is running");
     }
 }
